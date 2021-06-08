b5c2ad946273c9bc39292167eddbfa9dd4887bbd
CallMeFoxie <ashley@victorianfox.com>
2021-06-08 22:43:20 +0200

    Update kernel and curl

--- buildsteps/packages/006_curl/script.sh
+++ buildsteps/packages/006_curl/script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PKGVERSION=7.76.1
+PKGVERSION=7.77.0
 PKGNAME="curl"
 SOURCEFILE="curl-${PKGVERSION}.tar.xz"
-URL="https://github.com/curl/curl/releases/download/curl-7_76_1/"
+suburl=$(echo $PKGVERSION | sed 's/\./_/g')
+URL="https://github.com/curl/curl/releases/download/curl-${suburl}/"
 
 build() {
 	./configure ${DEFAULT_CONFIGURE_FLAGS} --with-ssl --disable-ipv6 --disable-unix-sockets --disable-verbose --without-libidn --with-zlib  --with-ca-bundle=/etc/ssl/ca-bundle.crt
